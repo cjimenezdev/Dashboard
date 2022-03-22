2392d174703d6cd738c43399b479b3ca4e0d422a
Carlos Jiménez <72168739+CJimenezDev@users.noreply.github.com>
2022-03-22 01:13:27 -0500

    Add files via upload

---
+++ app/js/app.js
@@ -0,0 +1,32 @@
+const menu = document.querySelector(".navbar-toggler");
+const toggler = document.querySelector(".btn-toggle");
+const moon_sun = document.querySelector(".moon_sun");
+
+const sidebarMenu = document.querySelector(".nav"),
+    sidebarLi = sidebarMenu.querySelectorAll(".nav-item"),
+    totalNav = sidebarLi.length;
+
+moon_sun.addEventListener("click", () => {
+    moon_sun.querySelector('.moon').classList.toggle("d-none");
+    moon_sun.querySelector('.sun').classList.toggle("d-none");
+    document.body.classList.toggle("dark");
+});
+menu.addEventListener("click", () => {
+    document.querySelector('.nav__bar').classList.add("show");
+    document.querySelector('.nav__bar').classList.remove("hide");
+});
+toggler.addEventListener("click", () => {
+    document.querySelector('.nav__bar').classList.add("hide");
+    document.querySelector('.nav__bar').classList.remove("show");
+});
+
+for (let i = 0; i < totalNav; i++) {
+    const a = sidebarLi[i].querySelector(".nav-link");
+    a.addEventListener("click", function() {
+
+        for (let j = 0; j < totalNav; j++) {
+            sidebarLi[j].querySelector(".nav-link").classList.remove("active");
+        }
+        this.classList.add("active");
+    })
+}
